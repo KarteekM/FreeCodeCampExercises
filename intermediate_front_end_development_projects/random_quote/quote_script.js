55c9705fcd2efb594c5de726b26bf149a44b05b1
Karteek M <mkarteek27@gmail.com>
2018-04-03 20:38:28 +0530

    Randomquote basic

--- intermediate_front_end_development_projects/random_quote/quote_script.js
+++ intermediate_front_end_development_projects/random_quote/quote_script.js
@@ -1,50 +1,10 @@
-
-/* function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } } */
-
-
-var currentQuote = '', currentAuthor = '';
-function openURL(url){
-  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
-}
 function getQuote() {
-	var currentQuote = '', currentAuthor = '';
-  $.ajax({
-    headers: {
-      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
-      Accept: "application/json",
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
-    success: function(r) {
-      if (typeof r === 'string') {
-       r = JSON.parse(r); 
-      }
-      currentQuote = r.quote;
-      currentAuthor = r.author;
-	}
-      /* if(inIframe())
-      {
-        $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-        $('#tumblr-quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote)+'&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
-      } */
-     /*  $(".quote-text").animate({
-          opacity: 0
-        }, 500,
-        function() {
-          $(this).animate({
-            opacity: 1
-          }, 500);
-          $('#text').text(r.quote);
-        }); */
-
-		
-
-    //  var color = Math.floor(Math.random() * colors.length);
-
-  }); */
-});
-}
-/* $(document).ready(function() {
-  getQuote();
- // $('#new-quote').on('click', getQuote);
-}); */
+var quotes = ["Hello" ,
+			  "This is the first quote", 
+			  "This is the second quote",
+			  "This is the third quote",
+              "This is the fourth quote",
+			  "This is the fifth quote"];
+ var generatedQuote= quotes[Math.ceil((Math.random()%6).toFixed(2))];  
+ document.getElementById('randomQuotebox').innerHTML=generatedQuote;
+}			  
